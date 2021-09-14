5702b29cd571971fe122c65681b62e5afc047847
Daryl Richter <daryl@deliverycircle.com>
2021-09-14 15:42:42 -0400

    now displaying a list of the ship's graphs!

--- lib/mars_base_10/comm_central.rb
+++ lib/mars_base_10/comm_central.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 require 'mars_base_10/viewport'
+require 'urbit/urbit'
 
 module MarsBase10
   class Error < StandardError; end
 
   class CommCentral
-    attr_accessor :viewport
+    attr_accessor :ship, :viewport
 
-    def initialize
-      @viewport = MarsBase10::ViewPort.new
+    def initialize(config_filename:)
+      @ship = Urbit.connect(config_file: config_filename)
+      @viewport = MarsBase10::ViewPort.new on_ship: @ship
     end
 
     def activate
--- lib/mars_base_10/viewport.rb
+++ lib/mars_base_10/viewport.rb
@@ -1,32 +1,42 @@
 # frozen_string_literal: true
 require 'curses'
+require 'sorted_set'
 
 module MarsBase10
-  class ViewPort
-    attr_accessor :draw_col, :draw_row, :index, :win
+  class ViewPortSubject
+    attr_accessor :cols, :contents, :rows
+    def initialize(on:)
+      @cols     = on.graph_names.inject(0) {|a, n| n.length > a ? n.length : a}
+      @contents = on.graph_names
+      @rows     = @contents.size
+    end
+  end
 
-    def initialize
+  class ViewPort
+    attr_accessor :draw_col, :draw_row, :index, :subject, :title, :win
+
+    def initialize(on_ship:)
       Curses.init_screen
       Curses.noecho   # Do not echo characters typed by the user.
       Curses.start_color if Curses.has_colors?
-      @win = Curses::Window.new(0,0,0,0)
       @draw_col = 1
       @draw_row = 1
       @index = 1
+      @subject = ViewPortSubject.new on: on_ship
+      @title = "Graphs"
     end
 
     def clear
+      @win = Curses::Window.new(self.subject.rows + 2, self.subject.cols + 6, 0, 0)
       self.win.box
+      self.win.setpos(0, 2)
+      self.win.addstr(" #{self.title} ")
       self.draw_row = 1
       self.draw_col = 1
     end
 
     def close
       Curses.close_screen
-    end
-
-    def contents
-      ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
     end
 
     def max_cols
@@ -41,9 +51,9 @@ module MarsBase10
       loop do
         self.clear
 
-        self.contents.each do |item|
+        self.subject.contents.each do |item|
           self.win.setpos(self.draw_row, self.draw_col)
-          self.win.addstr(item)  # Display the text
+          self.win.addstr("#{"%02d" % draw_row} #{item}")        # Display the row_number and then the text
           self.draw_row += 1
         end
 
@@ -76,12 +86,12 @@ module MarsBase10
     end
 
     #
-    # This is a no-op if the index is out of range
+    # this is a no-op if the index is out of range
     #
     def set_row(i)
       i = 10 if (0 == i)
       i = 1 if (11 == i)
-      self.index = i if (i <= self.contents.size) && (i > 0)
+      self.index = i if (i <= self.subject.rows) && (i > 0)
     end
   end
 end
