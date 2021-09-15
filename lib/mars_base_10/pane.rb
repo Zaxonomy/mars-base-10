3d8e1b103a206830794039f67962c6874b632fb0
Daryl Richter <daryl@deliverycircle.com>
2021-09-15 17:14:11 -0400

    scrolling and highlighting working

--- lib/mars_base_10/pane.rb
+++ lib/mars_base_10/pane.rb
@@ -16,14 +16,19 @@ module MarsBase10
 
     def display
       loop do
-        self.draw_border
+        self.prepare_for_writing_contents
 
         (0..(self.max_contents_rows - 1)).each do |item|
           self.win.setpos(self.draw_row, self.draw_col)
-          self.win.addstr("#{"%2d" % item} #{self.subject.contents[item]}")        # Display the row_number and then the text
+          # The string here is the gutter followed by the window contents. improving the gutter is tbd.
+          win.attron(Curses::A_REVERSE) if item == self.index
+          self.win.addstr("#{"%2d" % item} #{self.subject.at index: item}")
+          win.attroff(Curses::A_REVERSE) if item == self.index
+          self.win.clrtoeol
           self.draw_row += 1
         end
 
+        self.draw_border
         win.setpos(self.index + 1, self.first_col)
         win.refresh  # Refresh the screen
         self.process
@@ -33,13 +38,11 @@ module MarsBase10
     def draw_border
       self.win.box
       self.draw_title
-      self.draw_row = self.first_row
-      self.draw_col = self.first_col
     end
 
     def draw_title
       self.win.setpos(0, 2)
-      self.win.addstr(" #{self.subject.title} ")
+      self.win.addstr(" #{self.subject.title} (#{self.subject.rows} total) ")
     end
 
     def first_col
@@ -66,6 +69,11 @@ module MarsBase10
       [10, (self.subject.rows + 3)].min
     end
 
+    def prepare_for_writing_contents
+      self.draw_row = self.first_row
+      self.draw_col = self.first_col
+    end
+
     def process
       str = win.getch.to_s
       case str
@@ -88,8 +96,18 @@ module MarsBase10
     # this is a no-op if the index is out of range
     #
     def set_row(i)
-      i = 9 if (i < 0)
-      i = 0 if (i > 9)
+      self.subject.scroll_limit = self.max_contents_rows
+
+      if (i < 0)
+       self.subject.scroll_up
+       i = 0
+      end
+
+      if (i > 9)
+        self.subject.scroll_down
+        i = 9
+      end
+
       self.index = i if (i <= self.max_contents_rows) && (i >= 0)
     end
   end
--- lib/mars_base_10/subject.rb
+++ lib/mars_base_10/subject.rb
@@ -2,12 +2,29 @@
 
 module MarsBase10
   class Subject
-    attr_accessor :cols, :contents, :rows, :title
+    attr_accessor :first_row, :scroll_limit
+    attr_reader   :cols, :contents, :rows, :title
+
     def initialize(wrapping:)
-      @contents = wrapping.graph_names
-      @cols     = @contents.inject(0) {|a, n| n.length > a ? n.length : a}
-      @rows     = @contents.size
-      @title    = "Graphs"
+      @contents  = wrapping.graph_names
+      @cols      = @contents.inject(0) {|a, n| n.length > a ? n.length : a}
+      @rows      = @contents.size
+      @title     = "Graphs"
+      @first_row = 0
     end
+
+    # Returns the item at: the index: relative to the first_row.
+    def at(index:)
+      self.contents[self.first_row + index]
+    end
+
+    def scroll_down
+      self.first_row = [self.first_row + 1, (self.rows - self.scroll_limit)].min
+    end
+
+    def scroll_up
+      self.first_row = [self.first_row - 1, 0].max
+    end
+
   end
 end
