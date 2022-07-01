39acb9ccb5d1d8413f44075e3c5e2a51554ba7bd
Daryl Richter <daryl@deliverycircle.com>
2022-07-01 15:46:09 -0400

    scrolling working with messages.
    still super slow but works.

--- lib/mars_base_10/controller/social_lounge.rb
+++ lib/mars_base_10/controller/social_lounge.rb
@@ -6,11 +6,20 @@ module MarsBase10
   class SocialLounge < Controller
     attr_accessor :channel
 
+    def initialize(manager:, ship_connection:, viewport:)
+      @index_ary = []
+      super(manager: manager, ship_connection: ship_connection, viewport: viewport)
+    end
+
     def active_channel
       if self.channel.nil?
         self.channel = self.ship.fetch_channel(group_title: 'The Great North', channel_title: 'Top Shelf')
       end
       self.channel
+    end
+
+    def active_node_index
+      @index_ary[@pane_1.index]
     end
 
     def active_resource
@@ -25,25 +34,31 @@ module MarsBase10
       if @pane_1 == self.viewport.active_pane
         @pane_1.clear
         @pane_1.subject.title = "Messages in #{self.active_channel.title}"
-        @pane_1.subject.contents = self.nodes_to_messages
+        @pane_1.subject.contents = self.nodes_to_messages(count: @pane_1.last_visible_row)
       end
       nil
     end
 
     def load_history
       return 0 unless @pane_1 == self.viewport.active_pane
-      new_content = self.ship.fetch_older_nodes(resource: self.active_resource, node: self.active_node)
-      @pane_1.subject.prepend_content(ary: new_content)
-      new_content.length
+      new_content_indexes = self.ship.fetch_older_nodes(resource: self.active_resource, node: self.active_node, count: @pane_1.last_visible_row)
+      @index_ary =  new_content_indexes + @index_ary
+      messages = new_content_indexes.map do |i|
+        # Can't use fetch_node_contents because we're formatting differently now.
+        self.message(node: self.ship.fetch_node(resource: self.active_resource, index: i))
+      end
+      @pane_1.subject.prepend_content(ary: messages)
+      new_content_indexes.length
     end
 
     def message(node:)
       "~#{node.to_h[:author]}  #{node.to_h[:contents][0]["text"]}"
     end
 
-    def nodes_to_messages
-      indexes = self.ship.fetch_node_list(resource: self.active_resource, count: @pane_1.last_visible_row)
-      messages = indexes.map do |i|
+    def nodes_to_messages(count:)
+      node_indexes = self.ship.fetch_node_list(resource: self.active_resource, count: count)
+      messages = node_indexes.map do |i|
+        @index_ary << i
         # Can't use fetch_node_contents because we're formatting differently now.
         self.message(node: self.ship.fetch_node(resource: self.active_resource, index: i))
       end
