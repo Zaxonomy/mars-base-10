44f10aa079c3000b06854656f45c0fff89758b81
Daryl Richter <daryl@deliverycircle.com>
2022-07-26 13:55:19 -0400

    new approach to pane drawing
    the way i was modifying the drawing loop was bothering
    me for a while and this is a much cleaner approach.

--- lib/mars_base_10/pane.rb
+++ lib/mars_base_10/pane.rb
@@ -3,22 +3,24 @@ require 'curses'
 
 module MarsBase10
   class Pane
-    attr_accessor :draw_row, :draw_col, :highlight, :index, :latch, :subject
+    attr_accessor :cur_draw_row, :cur_draw_col, :extended_lines, :highlight, :index, :latch, :subject
     attr_reader   :height_pct, :left_edge_col, :top_row, :viewport, :width_pct
 
     def initialize(viewport:, at_row:, at_col:, height_pct: 1, width_pct: 1)
-      @top_row       = at_row
-      @left_edge_col = at_col
-      @height_pct    = height_pct
-      @highlight     = true
-      @index         = 1
-      @latch         = ''
-      @latch_depth   = 0
-      @subject       = nil
-      @win           = nil
-      @viewport      = viewport
-      @visible_content_shift = 0
-      @width_pct     = width_pct
+      @first_visible_index   = 1
+      @height_pct            = height_pct
+      @highlight             = false
+      @index                 = 1
+      @last_visible_index    = 0
+      @latch                 = ''
+      @latch_depth           = 0
+      @left_edge_col         = at_col
+      @subject               = nil
+      @top_row               = at_row
+      @win                   = nil
+      @viewport              = viewport
+      # @visible_content_shift = 0
+      @width_pct             = width_pct
     end
 
     def active?
@@ -29,11 +31,15 @@ module MarsBase10
       self.reset
       self.prepare_for_writing_contents
       (0..(self.last_row - 1)).each do |item|
-        self.window.setpos(self.draw_row, self.draw_col)
+        self.window.setpos(self.cur_draw_row, self.cur_draw_col)
         self.window.addstr("")
         self.window.clrtoeol
-        self.draw_row += 1
+        self.cur_draw_row += 1
       end
+    end
+
+    def current_item_index
+      (self.cur_draw_row - self.extended_lines) + self.visible_content_shift
     end
 
     def current_subject_index
@@ -43,40 +49,43 @@ module MarsBase10
     def draw
       self.prepare_for_writing_contents
 
-      first_draw_row = self.first_row
-      last_draw_row  = self.last_drawable_row
-      item_index = 0
-
-      (first_draw_row..last_draw_row).each do |index|
-        self.draw_line
-        item_index = index + @visible_content_shift
-        self.window.attron(Curses::A_REVERSE) if (item_index == self.index) && self.highlight
-
-        if self.subject.line_length_at(index: item_index) > self.last_col
-          chunks = self.subject.line_at(index: item_index).chars.each_slice(self.last_col - 2).map(&:join)
-          chunks.each do |c|
-            self.window.addstr(c)
-            self.draw_row += 1
-            self.draw_line
-            last_draw_row -= 1  # pull the last drawing row in 1 for each new line added.
+      while self.cur_draw_row <= self.last_drawable_row
+        if self.current_item_index <= self.subject.item_index_range.last
+          if (self.current_item_index == self.index) && !self.highlight
+            self.window.attron(Curses::A_REVERSE)
+            self.highlight = true
           end
-          self.draw_row -= 1
+
+          if self.subject.line_length_at(index: self.current_item_index) > self.last_col
+            chunks = self.subject.line_at(index: self.current_item_index).chars.each_slice(self.last_col - 2).map(&:join)
+            chunks.each do |c|
+              self.draw_line
+              self.window.addstr("#{c}")
+              self.cur_draw_row += 1
+              self.extended_lines += 1
+            end
+            self.extended_lines -= 1
+          else
+            self.draw_line
+            self.window.addstr("#{self.subject.line_at(index: self.current_item_index)}")
+            # self.window.addstr("#{@first_visible_index} #{@last_visible_index} #{self.subject.line_at(index: self.current_item_index)}")
+            self.cur_draw_row += 1
+          end
+
+          if self.highlight
+            self.window.attroff(Curses::A_REVERSE)
+            self.highlight = false
+          end
         else
-          # self.window.addstr("#{self.subject.line_at(index: item_index)}")
-          self.window.addstr("#{item_index} #{last_drawable_row} #{self.subject.line_at(index: item_index)}")
+          self.draw_line
+          self.window.addstr(" ")
+          self.cur_draw_row += 1
         end
-
-        self.window.attroff(Curses::A_REVERSE) if (item_index == self.index) && self.highlight
         self.window.clrtoeol
-        self.draw_row += 1
       end
-
-      # Adjust our frame if we have wrapped rows.
-      if item_index < last_drawable_row
-        @visible_content_shift -= (last_draw_row - item_index)
-      end
-
       self.draw_border
+
+      @last_visible_index = self.current_item_index - 1  # Subtract one b/c we have already incremented before loop
     end
 
     def draw_border
@@ -87,7 +96,7 @@ module MarsBase10
     end
 
     def draw_line
-      self.window.setpos(self.draw_row, self.draw_col)
+      self.window.setpos(self.cur_draw_row, self.cur_draw_col)
     end
 
     def draw_title
@@ -95,11 +104,11 @@ module MarsBase10
       self.window.addstr(" #{self.subject.title} (#{self.max_contents_rows} total) ")
     end
 
-    def first_col
+    def first_drawable_col
       1
     end
 
-    def first_row
+    def first_drawable_row
       1
     end
 
@@ -120,12 +129,15 @@ module MarsBase10
     end
 
     #
-    # This is the _relative_ last row, e.g. the height of the pane in columns.
+    # This is the _relative_ last row, e.g. the height of the pane in rows
     #
     def last_row
       (self.viewport.max_rows * self.height_pct).floor
     end
 
+    #
+    # This is the height of the pane minus the border
+    #
     def last_visible_row
       self.last_row - 2
     end
@@ -146,8 +158,11 @@ module MarsBase10
     end
 
     def prepare_for_writing_contents
-      self.draw_row = self.first_row
-      self.draw_col = self.first_col
+      self.cur_draw_row = self.first_drawable_row
+      self.cur_draw_col = self.first_drawable_col
+      self.extended_lines = 0
+      @first_visible_index = self.cur_draw_row + self.visible_content_shift
+      @last_visible_index = self.subject.item_count if @last_visible_index < @first_visible_index
     end
 
     #
@@ -193,7 +208,7 @@ module MarsBase10
       @index = 1
       @latch = ''
       @latch_depth = 0
-      @visible_content_shift = 0
+      # @visible_content_shift = 0
     end
 
     def right_pad
@@ -201,12 +216,21 @@ module MarsBase10
     end
 
     def scroll_to_row(index)
+      jump = [(index - self.index), (self.max_contents_rows - self.visible_content_range.last)].min
       if index > self.index
         # Scrolling down
-        @visible_content_shift = [(index - self.last_drawable_row), 0].max
+        # @visible_content_shift = [(index - @last_visible_index), 0].max
+        if index > self.visible_content_range.last
+          @first_visible_index = @first_visible_index + jump
+          @last_visible_index = @last_visible_index + jump
+        end
       else
         # Scrolling up
-        @visible_content_shift = [(index - self.first_row), 0].max
+        if index < self.visible_content_range.first
+            @first_visible_index = [(@first_visible_index + jump), 1].max
+            @last_visible_index = @last_visible_index + jump
+        end
+      #   @visible_content_shift = [(index - self.first_drawable_row), 0].max
       end
       [index, 1].max
     end
@@ -234,7 +258,12 @@ module MarsBase10
     end
 
     def visible_content_range
-      ((self.first_row + @visible_content_shift)..(self.last_drawable_row + @visible_content_shift))
+      # ((self.first_drawable_row + @visible_content_shift)..(self.last_drawable_row + @visible_content_shift))
+      (@first_visible_index..@last_visible_index)
+    end
+
+    def visible_content_shift
+      self.visible_content_range.first - self.first_drawable_row
     end
 
     def window
